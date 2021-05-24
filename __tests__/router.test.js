de5a02b2252bea174596725ebbc75db7cfe1c3db
JGuo24 <60760168+JGuo24@users.noreply.github.com>
2021-05-23 18:43:04 -0700

    part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,34 @@
  * @jest-environment jsdom
  */
 
+import { pushToHistory } from "../scripts/router";
+
+describe('test pushToHistory stack length', () =>{
+    
+    test('push settings is correct', () => {
+        let length = history.length;
+        expect(pushToHistory('settings', 0).length).toBe(length + 1);  
+    });
+
+    test('push entry is correct', () =>{
+        let length = history.length;
+        expect(pushToHistory('entry', 1).length).toBe(length + 1);
+    });
+
+    test('push null is correct', () =>{
+        let length = history.length;
+        expect(pushToHistory('','').length).toBe(length+1);
+    });
+})
+
+describe('test pushToHistory current state object', () =>{
+    test('settings page state is correct', () =>{
+        expect(pushToHistory('settings').state.page).toBe('settings');
+    });
+    test('entry page state is correct', () =>{
+        expect(pushToHistory('entry', 4).state.page).toBe('entry4');
+    });
+    test('default page state is correct', () =>{
+        expect(pushToHistory().state.page).toBe();
+    });
+})
